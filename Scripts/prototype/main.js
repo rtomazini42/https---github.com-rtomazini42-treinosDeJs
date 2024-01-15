84e1b49f0e8ac74331feae7b6ce794b2695fd097
renan tomazini <renantomazini@gmail.com>
2024-01-15 16:22:23 -0300

    classes

--- Scripts/prototype/main.js
+++ Scripts/prototype/main.js
@@ -12,3 +12,34 @@ const celta = {
 
 console.log(celta.material)
 
+//função construtora
+function Carro(nome, placa){
+    this.nome = nome;
+    this.placa = placa;
+}
+
+const carroNovo = new Carro('Fiat uno', 'vde-1234');
+console.log(carroNovo);
+
+
+Carro.prototype.andar = function(){
+    console.log(`Andando com ${this.nome}`);
+}
+
+carroNovo.andar();
+
+//outra maneira
+
+class Moto{
+    constructor(nome,placa){
+        this.nome = nome
+        this.placa = placa
+    }
+
+    andar(){
+        console.log(`Andando com ${this.nome}`);
+    }
+}
+
+const motoNova = new Moto('biz', 'aaa-1111');
+motoNova.andar();
