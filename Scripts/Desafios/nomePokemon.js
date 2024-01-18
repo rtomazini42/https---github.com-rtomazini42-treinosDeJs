7012df8405287a290340ff80dea68e805441e2ff
renan tomazini <renantomazini@gmail.com>
2024-01-18 02:39:28 -0300

    Treino completo

---
+++ Scripts/Desafios/nomePokemon.js
@@ -0,0 +1,11 @@
+var nomeEntrada = 'aaa';
+
+function combinandoNomesPokemons(nomeEntrada){
+  return nomeEntrada + saur;
+}
+
+// Chamando a função "combinandoNomesPokemons" com o nome pokemon informado e armazenando o resultado na variável "palavraGerada":
+var palavraGerada = combinandoNomesPokemons(nomeEntrada);
+
+// Exibindo a palavra gerada:
+print(palavraGerada);
